7bf3a26d3d415140079d74a7853c9ac4d7f1bcca
Onisodumeya Mazi <maziemmamazi@gmail.com>
2025-06-25 16:30:01 +0100

    Navbar mobile responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,63 @@
+import React, { useEffect, useState } from 'react';
 import { NavLink } from "react-router-dom";
 import { PriBtn, SecBtn } from '../components/Buttons.jsx'
+import Menu from '../assets/svgs/menu-btn.svg'
 
 
 
 function Navbar() {
+  const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const mediaQuery = window.matchMedia('(max-width: 900px)')
+
+        const handleMediaQuery = (event) => {
+            setIsMobile(event.matches)
+        }
+
+        setIsMobile(mediaQuery.matches);
+
+        mediaQuery.addListener(handleMediaQuery)
+
+        return () => mediaQuery.removeListener(handleMediaQuery)
+    }, [])
+
+
   return (
-    <nav className='flex justify-between py-5 px-10  items-center'>
+    <nav className='flex justify-between py-5 px-5 md:px-15  items-center'>
 
       <NavLink to="/" className='text-pink-400' ><h2 className='font-bold text-2xl'>WALLETBITS</h2></NavLink>
       
-      <div className='flex items-center gap-10'>
 
         {/* navbar links */}
-        <div className='flex gap-8'>
+        {!isMobile && 
+          <div className='flex items-center gap-10'>
+            <div className='flex gap-8'>
 
-          <NavLink to="/" className={({ isActive }) => isActive ? "font-medium border-b-2 border-black" : "border-b-2 border-transparent nav-links"}>Home</NavLink>
+              <NavLink to="/" className={({ isActive }) => isActive ? "font-medium border-b-2 border-black" : "border-b-2 border-transparent nav-links"}>Home</NavLink>
 
-          <NavLink to="/how-it-works" className={({ isActive }) => isActive ? "font-medium border-b-2 border-black" : "border-b-2 border-transparent nav-links transition-[0.3s]"}>How it Works</NavLink>
+              <NavLink to="/how-it-works" className={({ isActive }) => isActive ? "font-medium border-b-2 border-black" : "border-b-2 border-transparent nav-links transition-[0.3s]"}>How it Works</NavLink>
+
+              <NavLink to="/rates" className={({ isActive }) => isActive ? "font-medium border-b-2 border-black" : "border-b-2 border-transparent nav-links transition-[0.3s]"}>Rates</NavLink>
+
+              <NavLink to="/about-us" className={({ isActive }) => isActive ? "font-medium border-b-2 border-black" : "border-b-2 border-transparent nav-links transition-[0.3s]"}>About us</NavLink>
           
-          <NavLink to="/rates" className={({ isActive }) => isActive ? "font-medium border-b-2 border-black" : "border-b-2 border-transparent nav-links transition-[0.3s]"}>Rates</NavLink>
-          
-          <NavLink to="/about-us" className={({ isActive }) => isActive ? "font-medium border-b-2 border-black" : "border-b-2 border-transparent nav-links transition-[0.3s]"}>About us</NavLink>
-      
-        </div>
-      
-        {/* navbar buttons */}
-        <div className='flex gap-5 items-center'>
-      
-          <NavLink to="/sign-in"><SecBtn btnText="Sign in" /></NavLink>
-          <NavLink to="/register"><PriBtn btnText="Create Free Account" /></NavLink>
+            </div>
+
+            {/* navbar buttons */}
+            <div className='flex gap-5 items-center'>
+
+              <NavLink to="/sign-in"><SecBtn btnText="Sign in" /></NavLink>
+              <NavLink to="/register"><PriBtn btnText="Create Free Account" /></NavLink>
+
+            </div>
+          </div>
+
         
-        </div>
+        }
+
+        {isMobile && <img src={Menu} alt="" />}
         
-      </div>
     </nav>
   );
 }
