77497352f7b1e4bb4d526f8e034cf48756c6c0c7
Onisodumeya Mazi <maziemmamazi@gmail.com>
2025-06-27 15:10:43 +0100

    Footer Update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,6 @@
+import { Link } from 'react-router-dom'
+import { useMediaQuery } from 'react-responsive'
+
 import pngx from '../assets/pngs/X.png'
 import pngtube from '../assets/pngs/Youtube.png'
 import pnglink from '../assets/pngs/linked.png'
@@ -7,64 +10,70 @@ import facebook from '../assets/pngs/Facebook.png'
 
 
 function Footer() {
+  const isMobile = useMediaQuery({ maxWidth: 1000 });
+
+
   return (
-    <footer className="foot">
-      <div className="container" >
-        <div className="setions" id="fir">
-        <h1 className="head">WalletBits</h1>
-        <p>
-          Join our newsletter to get notified about all new releases and features.
-        </p>
-        <form>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            placeholder="Enter your email"
-            required
-          />
-          <button type="submit" id="btn">Subscribe</button>
-          
-        </form>
-        <p>
-          By subscribing, you agree to our <a href="#">Privacy Policy</a> <br /> and
-          provide consent to receive updates from our company.
-        </p>
-        </div>
-        <div className="sections" id="sec">
+    <footer className={`${isMobile ? 'pt-10' : 'pt-20'} px-5 md:px-15`}>
+      <div className="foot rounded-t-4xl">
+
+        <div className={`${isMobile ? 'flex-col' : 'flex-row'} flex  border border-green-500  w-full p-8 gap-10 justify-between`}>
+          <div className={`${isMobile ? 'w-full' : 'w-1/2'} sections flex flex-col w-1/2" id="fir`}>
+            <h1 className="head mb-5">WALLETBITS</h1>
+            <p className="mb-5 text-gray-50 text-[14px]">Join our newsletter to get notified about all new releases and features.</p>
+            <form className="mb-5">
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Enter your email"
+                required
+              />
+              <button type="submit" id="btn">Subscribe</button>
+
+            </form>
+            <p className="text-[14px]">
+              By subscribing, you agree to our <Link tp="#"><u>Privacy Policy</u></Link> and
+              provide consent to receive updates from our company.
+            </p>
+          </div>
+          <div className={`${isMobile ? 'w-full justify-between' : 'w-2/5'} sections w-2/5" id="sec`}>
             <div>
-          <h2 className="head">Company</h2>
-          <ul>
-            <li>About Us</li>
-            <li>Careers</li>
-            <li>Services</li>
-            <li>Blogs</li>
-            <li>CSR</li>
-          </ul>
-        </div>
+              <h2 className="head mb-4">Company</h2>
+              <ul className="flex flex-col gap-3">
+                <li><Link to="/about-us">About Us</Link></li>
+                <li><Link to="/careers">Careers</Link></li>
+                <li><Link to="/services">Services</Link></li>
+                <li><Link to="/blogs">Blogs</Link></li>
+                <li><Link to="/csr">CSR</Link></li>
+              </ul>
+            </div>
 
-        <div>
-          <h2 className="head">Support</h2>
-          <ul>
-            <li>FAQs</li>
-            <li>Contact</li>
-          </ul>
-        </div>
+            <div>
+              <h2 className="head mb-4">Support</h2>
+              <ul className="flex flex-col gap-3">
+                <li><Link to="/faqs">FAQs</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
+              </ul>
+            </div>
 
-        <div>
-          <h2 className="head">Follow Us</h2>
-          <ul>
-            <li><img src={facebook} /> Facebook</li>
-            <li><img src={pnginsta} /> Instagram</li>
-            <li><img src={pngx} /> X (Twitter)</li>
-            <li><img src={pnglink} /> LinkedIn</li>
-            <li><img src={pngtube}/> YouTube</li>
-          </ul>
+            <div>
+              <h2 className="head mb-4">Follow Us</h2>
+              <ul className="flex flex-col gap-3">
+                <li className="flex items-center gap-2"><img src={facebook} /> Facebook</li>
+                <li className="flex items-center gap-2"><img src={pnginsta} /> Instagram</li>
+                <li className="flex items-center gap-2"><img src={pngx} /> X (Twitter)</li>
+                <li className="flex items-center gap-2"><img src={pnglink} /> LinkedIn</li>
+                <li className="flex items-center gap-2"><img src={pngtube} /> YouTube</li>
+              </ul>
+            </div>
+          </div>
         </div>
-     </div>
+        <div className={`${isMobile ? 'flex-col items-center' : 'flex-row'} flex gap-2.5 justify-between w-full`}>
+          <p>&copy; {new Date().getFullYear()} Jodna Technologies. All rights reserved.</p>
+          <p> <a href="">privacy policy</a> <a href="">terms of service</a> <a href="">cookies setting</a></p>
+        </div>
       </div>
-      <p>&copy; {new Date().getFullYear()} Jodna Technologies. All rights reserved.</p>
-      <p> <a href="">privacy policy</a> <a href="">terms of service</a> <a href="">cookies setting</a></p>
     </footer>
   );
 }
