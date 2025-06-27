ad1496f511a31efb092f75d352bb047bcf34f22d
Onisodumeya Mazi <maziemmamazi@gmail.com>
2025-06-27 06:30:36 +0100

    Card update

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,21 +1,22 @@
 import PropTypes from 'prop-types';
+import { PriBtn } from './Buttons';
 
 function Cards({ image, title, description, buttonText }) {
   return (
     <>
-    <div className="card">
+    <div className="cards px-5 gap-5 py-10">
       <h1 className="header">{title}</h1>
       <img src={image} alt="Card visual" />
       <p>{description}</p>
       <button className="cardbutton">{buttonText}</button>
     </div>
     <div className='desk'>
-        <div className="divs">
+        <div className="divs gap-2 items-start">
             <h1 className="header">{title}</h1>
             <p>{description}</p>
             <button className="cardbutton">{buttonText}</button>
         </div> 
-        <div  className="divs"><img src={image} alt="image" className="img"/></div>
+        <img src={image} alt="image" className='card-img' />
     </div>
     </>
   );
@@ -27,16 +28,6 @@ Cards.propTypes = {
   title: PropTypes.string,
   description: PropTypes.string,
   buttonText: PropTypes.string,
-};
-
-
-Cards.defaultProps = {
-  title: 'Why Choose Walletbits',
-  description: `Walletbits is a smart choice for everyone looking to exchange gift cards 
-quickly, securely, and at competitive rates. Walletbits makes the entire process seamless—
-whether you are trading Amazon, iTunes, Steam, or other popular gift cards. Customers can enjoy 
-instant payouts into their Nigerian bank accounts in naira—backed by 24/7 customer support.`,
-  buttonText: 'Check Rates',
 };
 
 export default Cards;
