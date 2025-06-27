73e6d6f4292cf9cb149e803eb979129b484186d4
Onisodumeya Mazi <maziemmamazi@gmail.com>
2025-06-27 06:23:32 +0100

    Cards update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,11 @@ import { PriBtn, SecBtn } from "../components/Buttons.jsx";
 import CountUp from 'react-countup'
 import Hero from '../assets/pngs/hero-image.png'
 import Navbar from "../components/Navbar.jsx";
+import Cards from '../components/Cards.jsx'
+
+import image from '../assets/pngs/img1.png'
+import imag from '../assets/pngs/img2.png'
+import imags from '../assets/pngs/img3.png'
 
 function Home() {
   const isMobile = useMediaQuery({ maxWidth: 767 });
@@ -71,6 +76,21 @@ function Home() {
           </div>
 
         }
+      </section>
+
+      <section className="flex flex-col items-center gap-5 px-5 md:px-15 pt-10">
+        <Cards image={image}
+          title=" why choose walletbits?"
+          description= "Walletbits is a smart choice for everyone looking to exchange gift cards quickly, securely, and at competitive rates. Walletbits makes the entire process seamless—whether you are trading Amazon, iTunes, Steam, or other popular gift cards. Customers can enjoy instant payouts into their Nigerian bank accounts in naira—backed by 24/7 customer support."
+          buttonText="Register Now"/> 
+        <Cards image={imag}
+          title="Best Transaction Rates"
+          description="When you choose walletbits, you dont just exchange gift cards, you unlock nigeria's most competitive rate on the market.we constantly benchmark our exchange rate against leading platforms like cardtonic, apexpay and our rate consistently comes out ahead of others. that means more value to your unused Amazon, itunes, goggle play or other gift cards"
+          buttonText="Check Rates"/>
+        <Cards image={imags}
+          title="Secure and hassle free"
+          description="Walletbits offers secure and hassle free experience to everyone looking to exchange giftcards with confidence.your transactions are protected with industry-standard encryption and multi-layered verification, ensuring your data and funds remains safe at all times. from card submission to instant payout,everytime happens quickly and transparently"
+          buttonText="Start Trading"/>
       </section>
     </>
   )
