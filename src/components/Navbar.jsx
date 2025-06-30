ab1f15d490fa29d381ca7681818809f76ac46041
Onisodumeya Mazi <maziemmamazi@gmail.com>
2025-06-30 20:56:41 +0100

    Navbar animation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,27 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useMediaQuery } from 'react-responsive';
 import { NavLink } from "react-router-dom";
-import { PriBtn, SecBtn } from '../components/Buttons.jsx'
-import Menu from '../assets/svgs/menu-btn.svg'
+import { PriBtn, SecBtn } from '../components/Buttons.jsx';
+
+import { gsap, ScrollTrigger } from 'gsap/all'
+
+import Menu from '../assets/svgs/menu-btn.svg';
 
 
 
 function Navbar() {
+  const navRef = useRef(null);
+  const heroRef = useRef(null)
+
+  useEffect(() => {
+    if (navRef.current) {
+      gsap.fromTo(navRef.current,
+        { y: -50, opacity: 1 },
+        { y: 0, opacity: 1, duration: 1, ease: "power3.out" }
+      );
+    }
+  }, []);
+
   const isMobile = useMediaQuery({ maxWidth: 1000 });
 
   const [menuOpen, setMenuOpen] = useState(false);
@@ -18,61 +33,61 @@ function Navbar() {
   useEffect(() => {
 
     const menuBtn = document.querySelector('.menu-btn')
-  
-  if (menuOpen) {
-    document.body.style.overflow = 'hidden';
-    menuBtn?.classList.add("bg-gray-100")
-  } else {
-    document.body.style.overflow = '';
-    menuBtn?.classList.remove("bg-gray-100")
-  }
 
-  return () => {
-    document.body.style.overflow = '';
-    menuBtn?.classList.remove("bg-gray-100")
-  };
-}, [menuOpen]);
+    if (menuOpen) {
+      document.body.style.overflow = 'hidden';
+      menuBtn?.classList.add("bg-gray-100")
+    } else {
+      document.body.style.overflow = '';
+      menuBtn?.classList.remove("bg-gray-100")
+    }
+
+    return () => {
+      document.body.style.overflow = '';
+      menuBtn?.classList.remove("bg-gray-100")
+    };
+  }, [menuOpen]);
 
 
   return (
-    <nav className='flex justify-between py-5 px-5 md:px-15  items-center relative w-full'>
+    <nav ref={navRef} className='top-0 opacity-0 -translate-y-8 transition-all duration-500 z-50 flex justify-between py-5 px-5 md:px-15 items-center w-full'>
 
       <NavLink to="/" className='text-pink-400' ><h2 className='font-bold text-2xl'>WALLETBITS</h2></NavLink>
-      
 
-        {/* navbar links */}
-        {!isMobile && 
-          <div className='flex items-center gap-10'>
-            <div className='flex gap-8'>
 
-              <NavLink to="/" className={({ isActive }) => isActive ? "font-medium border-b-2 border-black" : "border-b-2 border-transparent nav-links"}>Home</NavLink>
+      {/* navbar links */}
+      {!isMobile &&
+        <div className='flex items-center gap-10'>
+          <div className='flex gap-8'>
 
-              <NavLink to="/how-it-works" className={({ isActive }) => isActive ? "font-medium border-b-2 border-black" : "border-b-2 border-transparent nav-links transition-[0.3s]"}>How it Works</NavLink>
+            <NavLink to="/" className={({ isActive }) => isActive ? "font-medium border-b-2 border-black" : "border-b-2 border-transparent nav-links"}>Home</NavLink>
 
-              <NavLink to="/rates" className={({ isActive }) => isActive ? "font-medium border-b-2 border-black" : "border-b-2 border-transparent nav-links transition-[0.3s]"}>Rates</NavLink>
+            <NavLink to="/how-it-works" className={({ isActive }) => isActive ? "font-medium border-b-2 border-black" : "border-b-2 border-transparent nav-links transition-[0.3s]"}>How it Works</NavLink>
 
-              <NavLink to="/about-us" className={({ isActive }) => isActive ? "font-medium border-b-2 border-black" : "border-b-2 border-transparent nav-links transition-[0.3s]"}>About us</NavLink>
-          
-            </div>
+            <NavLink to="/rates" className={({ isActive }) => isActive ? "font-medium border-b-2 border-black" : "border-b-2 border-transparent nav-links transition-[0.3s]"}>Rates</NavLink>
 
-            {/* navbar buttons */}
-            <div className='flex gap-5 items-center'>
+            <NavLink to="/about-us" className={({ isActive }) => isActive ? "font-medium border-b-2 border-black" : "border-b-2 border-transparent nav-links transition-[0.3s]"}>About us</NavLink>
 
-              <NavLink to="/sign-in"><SecBtn btnText="Sign in" /></NavLink>
-              <NavLink to="/register"><PriBtn btnText="Create Free Account" /></NavLink>
-
-            </div>
           </div>
 
-        
-        }
+          {/* navbar buttons */}
+          <div className='flex gap-5 items-center'>
 
-        {isMobile && 
-        
+            <NavLink to="/sign-in"><SecBtn btnText="Sign in" /></NavLink>
+            <NavLink to="/register"><PriBtn btnText="Create Free Account" /></NavLink>
+
+          </div>
+        </div>
+
+
+      }
+
+      {isMobile &&
+
         <>
-          <img className='menu-btn p-2 rounded-md' src={Menu} alt="" onClick={toggleMenu}/>
-          
-          { menuOpen &&
+          <img className='menu-btn p-2 rounded-md' src={Menu} alt="" onClick={toggleMenu} />
+
+          {menuOpen &&
 
             <div className='flex flex-col gap-8 absolute -bottom-80 p-5  left-0 shadow-md  overflow-hidden bg-white w-full'>
 
@@ -89,15 +104,15 @@ function Navbar() {
                 <NavLink to="/sign-in"><SecBtn btnText="Sign in" /></NavLink>
                 <NavLink to="/register"><PriBtn btnText="Create Free Account" /></NavLink>
 
+              </div>
+
             </div>
-          
-          </div>
 
           }
         </>
-         
-        }
-        
+
+      }
+
     </nav>
   );
 }
