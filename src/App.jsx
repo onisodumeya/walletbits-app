a5c137d0cbcb9ef6ef3d9091fbf531caac99f136
Onisodumeya Mazi <maziemmamazi@gmail.com>
2025-07-01 00:51:20 +0100

    New pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
-import Footer from './components/Footer.jsx'
+import About from './pages/AboutUs.jsx'
+import HowItWorks from './pages/HowItWorks.jsx';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about-us" element={<About />} />
+        <Route path="/how-it-works" element={<HowItWorks />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/HowItWorks.jsx
@@ -0,0 +1,19 @@
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+
+function HowItWorks() {
+
+    return (
+        <>
+            <Navbar />
+
+            {/* Elements should be placed here */}
+
+            <Footer />
+        </>
+    )
+
+}
+
+export default HowItWorks
