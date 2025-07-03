d48a14a167ebd1f796de3c5b987df47a5cc37488
Onisodumeya Mazi <maziemmamazi@gmail.com>
2025-07-03 17:27:32 +0100

    Sign in page update

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -7,35 +7,7 @@ function SignIn(){
     return(
         <div className='flex h-screen bg-[#F9FAFB]'>
             <div className='w-1/2 flex items-center justify-center'>
-                <AuthForm 
-
-                    heading="Log In"
-                    subHeading="Log in and start exploring"
                 
-                    inputs={
-                        <>
-
-                            <Input
-
-                                labelText="Email Address"
-                                placeholder="moses12345@gmail.com"
-
-                            />
-
-                            <Input
-
-                                labelText="Password"
-                                name="password"
-
-                            />
-                        
-                        </>
-                    }
-
-                    buttonText="Sign In"
-
-                    forgotPasswordText="Forgot your password?"
-                />
             </div>
             <ImageBg />
         </div>
