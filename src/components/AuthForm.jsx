baeaa1f7455f95af34f83fd86d4e72dfcab56e41
Onisodumeya Mazi <maziemmamazi@gmail.com>
2025-07-04 22:07:04 +0100

    AuthForm update

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -1,13 +1,15 @@
-import { Link } from "react-router-dom"
+import Arrow from '../assets/svgs/previous-arrow.svg'
+import { Link, useNavigate } from "react-router-dom"
 
-export const AuthForm = ({ method, heading, subHeading, inputs, buttonText, redirectText, linkText, link, buttonLink, forgotPasswordText, extra }) => {
+export const AuthForm = ({ method, heading, subHeading, inputs, buttonText, buttonLink, hasAccount = false, noAccount = false, forgotPassword = false, extra }) => {
 
     return(
-        <form method={method} className="flex flex-col shadow-md items-center w-4/5 bg-white px-8 py-8 rounded-[20px] gap-6">
-            <div className="self-start">
-                <img src="" alt="" />
+        <form method={method} className="flex flex-col shadow-md items-center w-4/5 bg-white px-8 py-6 rounded-[20px] gap-4">
+            <button className="self-start flex gap-2.5 cursor-pointer" type='button' onClick={(e) => {e.preventDefault();
+                window.history.back()}}>
+                <img src={Arrow} alt="" className='w-4'/>
                 <p>Back</p>
-            </div>
+            </button>
 
             <div className="flex flex-col gap-4">
                 <h1 className="text-center place-items-center text-4xl">{heading}</h1>
@@ -28,20 +30,26 @@ export const AuthForm = ({ method, heading, subHeading, inputs, buttonText, redi
                 </button>
             </Link>
 
-            {redirectText && <p>{redirectText}{" "}<Link to={link} className="text-[#D470B7]">{linkText}</Link></p>}
-            {forgotPasswordText && <Link to={link}><u>{forgotPasswordText}</u></Link>}
+            {hasAccount && <p>Already have an account?{" "}<Link to='/sign-in' className="text-[#D470B7]">Login</Link></p>}
+            {noAccount && <p>Don't have an account?{" "}<Link to='/register' className="text-[#D470B7]">Sign up</Link></p>}
+            {forgotPassword && <Link to='/forgot-password'><u>Forgot your password?</u></Link>}
         </form>
     )
 
 }
 
-export const Input = ({ name, labelText, placeholder, type = "text", value, onChange}) => {
+export const Input = (props) => {
+    
 
     return(
         <div className="flex flex-col gap-2">
-            <label htmlFor={name} className="text-xs font-medium">{labelText}</label>
-            <input type={name} id={name} placeholder={placeholder} value={value} onChange={onChange} className="border w-full border-[#E6E6E6] px-5 py-3 outline-0 text-sm rounded-xl"/>
+            <label htmlFor={props.id} className="text-xs font-medium">{props.labelText}</label>
+            <input type={props.inputType} id={props.id} placeholder={props.placeholder} value={props.value} onChange={props.onChange} className="border w-full border-[#E6E6E6] px-5 py-3 outline-0 text-sm rounded-xl"/>
         </div>
     )
 
+}
+
+Input.defaultProps = {
+    type: 'text'
 }
