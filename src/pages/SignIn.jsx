acf8a8e2469a61372bf1e977ce6c62fbb7ce241f
dhonnie04 <koweidonald@gmail.com>
2025-07-05 17:59:39 +0100

    signin

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -7,7 +7,38 @@ function SignIn(){
     return(
         <div className='flex h-screen bg-[#F9FAFB]'>
             <div className='w-1/2 flex items-center justify-center'>
+                <AuthForm>
+                    heading = "Log In"
+                    subHeading = "Log in and start exploring"
+
+                    inputs={
+                        <>
+                            <Input
+                                labelText={"Email"}
+                                placeholder={"Email address"}
+                                name={"emailAddress"}
+                                inputType={"email"}
+                            />
+
+                            <Input
+                                labelText={"Password"}
+                                placeholder={"Password"}
+                                name={"loginPassword"}
+                                inputType={"password"}
+                            />
+                        </>
+                    }
+                </AuthForm>
                 
+                buttonText = "Log In"
+
+                buttonLink = ""
+
+                bottomLinkText = "Forgot password?"
+
+                noAccount = {true}
+
+                forgotPassword = {true}
             </div>
             <ImageBg />
         </div>
