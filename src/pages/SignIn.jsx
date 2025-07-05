010b0d5db5b0c4f1ccf3cb58fc221a97573fc18d
Onisodumeya Mazi <maziemmamazi@gmail.com>
2025-07-05 18:33:09 +0100

    Sign in page update

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -50,7 +50,7 @@ function SignIn(){
 
                     buttonText = "Log in"
 
-                    buttonLink = ""
+                    buttonLink = {}
 
                     noAccount = {true}
 
